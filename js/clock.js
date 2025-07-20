4521140e997de0f23a26a4dc0e3a334c5d21d2fb
Basil531 <130035457+Basil531@users.noreply.github.com>
2025-07-19 21:40:37 -0400

    new clock format

--- js/clock.js
+++ js/clock.js
@@ -3,4 +3,28 @@ function amaliTime() {
 
     let timeDisplay = document.getElementById("time");
     timeDisplay.textContent = d.toLocaleTimeString("es-ES", {timeZone: "Pacific/Niue"});;
-    }
+    };
+
+function newAmaliTime() {
+
+    // Define dict to convert to amali suffix
+    let s = {
+      'am' : 'n',
+      'pm' : 'd'
+    };
+
+    //get current date/time
+    let d = new Date;
+    
+    //func to put time in format XX:XX:XX d/n
+    let f = d.toLocaleTimeString("en-PK", {timeZone: "Europe/Lisbon"}).split(" ");
+    let time = f[0];
+    let suffix = s[f[1]];
+    //console.log(time+s);
+
+    //send time to html
+    let timeDisplay = document.getElementById("time");
+    timeDisplay.textContent = time
+    let suffixDisplay = document.getElementById("suffix")
+    suffixDisplay = suffix
+};
